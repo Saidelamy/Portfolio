81a1e8cd6f75b24c89ef78c95df7e02fc3f72bbd
Saidelamy <saidmagdy511@gmail.com>
2023-12-26 14:46:02 +0200

    error handle

--- src/pages/home/Projects/Project1.jsx
+++ src/pages/home/Projects/Project1.jsx
@@ -3,7 +3,7 @@ import Heading from '../../../ui/Heading';
 import NumProject from '../../../ui/NumProject';
 import Paragraph from '../../../ui/Paragraph';
 import SliderImageRightToLeft from '../../../ui/SliderImageRightToLeft';
-import Image from '../../../ui/image';
+import Image from '../../../ui/Image';
 
 function Project1() {
   return (
